184c338c6d00e6721e5cfb3ebb4df851061d3980
Buck Golemon <buck@yelp.com>
2014-07-24 17:03:38 -0700

    mocha test configuration

--- tasks/simplemocha.js
+++ tasks/simplemocha.js
@@ -1,5 +1,12 @@
 module.exports = {
-  options: {
-    dest: 'CHANGELOG.md'
-  }
+    all: {
+      src: ['test/**/*.js'],
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'nyan'
+      }
+    }
 }
